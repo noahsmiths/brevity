d2cf776066c6ff0da05fc1c5460e27de05723daa
NSGOmega <emersontrickz@gmail.com>
2022-11-05 14:05:04 -0400

    Update index.js

--- frontend/src/components/SettingsNav/index.js
+++ frontend/src/components/SettingsNav/index.js
@@ -1,18 +1,17 @@
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function SettingsNav() {
 
-    const feather = require('feather-icons')
-    const history = useHistory();
+    const navigate = useNavigate();
 
     return (
 
         <main >
             <div className="navbar bg-base-200">
                 <div className="flex-1">
-                    <div className="btn btn-ghost"><i class="fa-solid fa-arrow-left"></i></div>
-                    <button onClick={() => history.push("/")} className="btn btn-ghost normal-case text-xl">Settings</button>
+                    <div onClick={() => navigate("/")} className="btn btn-ghost"><i class="fa-solid fa-arrow-left"></i></div>
+                    <div className="btn btn-ghost normal-case text-xl">Settings</div>
                 </div>
             </div>
         </main>
