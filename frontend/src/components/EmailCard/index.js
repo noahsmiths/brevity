e84f401d1b132ca2de20d7c37e96b49054a330fb
NSGOmega <emersontrickz@gmail.com>
2022-11-05 14:54:55 -0400

    Update index.js

--- frontend/src/components/EmailCard/index.js
+++ frontend/src/components/EmailCard/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function EmailCard(props) {
   const subject = props.subject;
@@ -9,16 +9,16 @@ export default function EmailCard(props) {
   var color;
   if (score > 0) {
     color = "#4BB543";
-  } else if (score == 0) {
+  } else if (score === 0) {
     color = "#B8B8B8";
   } else {
     color = "#FF9494";
   }
 
   return (
-    <div className="card w-96 bg-base-100 bg-white p-3 flex-row hover:underline hover:cursor-pointer">
+    <div className="card w-96 p-3 flex-row hover:underline hover:cursor-pointer">
       <div class="w-11 pr-2">
-        <div class="rounded-full h-9 w-9" style={{ backgroundColor: color}}>&nbsp;</div>
+        <div class="rounded-full h-9 w-9" style={{ backgroundColor: color }}>&nbsp;</div>
       </div>
       <div class="bg-flex">
         <p class="text-lg text-black">{props.subject}</p>
