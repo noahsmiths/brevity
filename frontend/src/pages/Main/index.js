f9a866a407b7e130628187e73818e04a7c81fa22
bgoulet1 <bgoulet@3cisd.com>
2022-11-05 14:58:43 -0400

    n/a

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,9 +1,11 @@
+import EmailCard from "../../components/EmailCard";
 import NavBar from "../../components/MainNav";
 
 export default function Main() {
   return (
     <div>
       <NavBar />
+      <EmailCard />
     </div>
   );
 }
