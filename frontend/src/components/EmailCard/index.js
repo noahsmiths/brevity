07344fdeb7eb26f942a9219311aa89784e6b48a0
Farouk Charkas <farouk.charkas@gmail.com>
2022-11-05 16:04:59 -0400

    styling the cards

--- frontend/src/components/EmailCard/index.js
+++ frontend/src/components/EmailCard/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 export default function EmailCard(props) {
-  const subject = props.subject;
-  const summary = props.summary;
-  const score = props.score;
-  const normMagnitude = props.normMangitude;
+  const { subject = "Email Subject" } = props;
+  const { summary = "Email Summary" } = props;
+  const { score = 0 } = props;
+  const { normMagnitude = 1.00 } = props;
+  const { link = "" } = props;
 
   var color;
   if (score > 0) {
@@ -16,16 +17,18 @@ export default function EmailCard(props) {
   }
 
   return (
-    <div className="card w-full mt-2 bg-base-300 scale-95 shadow-xl p-3 flex-row hover:underline hover:cursor-pointer hover:scale-100">
-      <div class="w-11 pr-2">
-        <div class="rounded-full h-9 w-9" style={{ backgroundColor: color }}></div>
-      </div>
-      <div class="bg-flex">
-        <p class="text-lg text-black">Example subject</p>
-        <p class="text-sm text-gray-700">Example summary</p>
-        <div>
+    <a href={link} target="_blank">
+      <div className="card w-full mt-1 bg-base-100 scale-95 shadow-md p-3 flex-row hover:underline hover:cursor-pointer border-0.2 border-softgray">
+        <div class="w-11 pr-2 flex items-center justify-center">
+          <div class="rounded-full h-9 w-9" style={{ backgroundColor: color }}></div>
+        </div>
+        <div class="bg-flex">
+          <p class="text-lg text-black">{subject}</p>
+          <p class="text-sm text-gray-700">{summary}</p>
+          <div>
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 }
