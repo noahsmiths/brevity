9dda7c15ba8359c47308ec3d0039f124f33d0a81
noah <90880131+noahsmiths@users.noreply.github.com>
2022-11-06 01:38:49 -0500

    drop down done wth a little refactor too

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -9,44 +9,65 @@ export default function Main() {
 
   const [emailData, setEmailData] = useState([]);
   const [hasEmailData, setHasEmailData] = useState(false);
+  const [sortMethod, setSortMethod] = useState('urgency');
+
+  const getEmailData = () => {
+    if (chrome?.storage?.local) {
+      chrome.storage.local.get(['emails'], (result) => {
+        if (result?.emails?.length > 0) {
+          // Do what you will with emails here
+          /*
+          // Code to sort by urgency
+          let emails = result.emails;
+          console.log(emails[0].urgency);
+          emails.sort((a, b) => {
+            return b.urgency - a.urgency;
+          });
+          setEmailData(emails);
+          */
+          setEmailData(result.emails);
+          setHasEmailData(true)
+          console.log(result);
+        } else {
+          setHasEmailData(false);
+          console.warn("Data couldn't be loadedß")
+        }
+      });
+    }
+  }
+
+  const setSort = (sort) => {
+    setSortMethod(sort);
+  }
 
   useEffect(() => {
-    function getEmailData() {
-      if (chrome?.storage?.local) {
-        chrome.storage.local.get(['emails'], (result) => {
-          if (result?.emails?.length > 0) {
-            // Do what you will with emails here
-            /*
-            // Code to sort by urgency
-            let emails = result.emails;
-            console.log(emails[0].urgency);
-            emails.sort((a, b) => {
-              return b.urgency - a.urgency;
-            });
-            setEmailData(emails);
-            */
-            setEmailData(result.emails);
-            setHasEmailData(true)
-            console.log(result);
-          } else {
-            setHasEmailData(false);
-            console.warn("Data couldn't be loadedß")
-          }
-        });
-      }
-    }
-
     getEmailData();
-  }, [])
+  }, []);
 
+  useEffect(() => {
+    if (sortMethod === 'urgency') {
+      let newEmailData = [...emailData];
 
+      newEmailData.sort((a, b) => {
+        return b.urgency - a.urgency;
+      });
 
+      setEmailData(newEmailData);
+    } else if (sortMethod === 'recency') {
+      let newEmailData = [...emailData];
 
+      newEmailData.sort((a, b) => {
+        return b.timestamp - a.timestamp;
+      });
+
+      setEmailData(newEmailData);
+    }
+  }, [sortMethod]);
 
   return (
     <div className="p-3 pr-5">
       <NavBar />
-      <Dropdown />
+      <Dropdown setSort={setSort} sortMethod={sortMethod}/>
       {emailData ? emailData.map((email, index) => {
         console.log(`key: ${index} \n pair: ${email}`);
         console.log(email);
