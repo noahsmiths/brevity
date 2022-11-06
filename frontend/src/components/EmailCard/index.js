a46aa12e4a8cffc2e1f30780ad8c0229639a198f
bgoulet1 <bgoulet@3cisd.com>
2022-11-06 00:22:18 -0400

    merging with em

--- frontend/src/components/EmailCard/index.js
+++ frontend/src/components/EmailCard/index.js
@@ -1,46 +1,51 @@
 import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
+// import format from "date-fns/format";
 
 export default function EmailCard({ props }) {
-
-  console.log(props)
+  console.log(props);
 
   const calculateUrgency = (urgency) => {
     switch (urgency) {
       case 1:
-        return ("bg-base-300");
+        return "bg-base-300";
       case 2:
-        return ("bg-yellow-500");
+        return "bg-yellow-500";
       case 3:
-        return ("bg-amber-500");
+        return "bg-amber-500";
       case 4:
-        return ("bg-orange-500");
+        return "bg-orange-500";
       case 5:
-        return ("bg-red-500");
+        return "bg-red-500";
       default:
-        return ("bg-base-300");
+        return "bg-base-300";
     }
-
-  }
+  };
 
   return (
     <a href={props.url}>
       <div className="card w-full mt-1 bg-base-ß100 border-0.2 border-softgray shadow-xl p-3 flex flex-row scale-[.975] hover:scale-100 hover:underline hover:cursor-pointer">
         <div class="w-11 pr-2">
-          <div class={`rounded-full mt-6 h-6 w-6 ml-1 ${calculateUrgency(props.urgency)}`}></div>
+          <div
+            class={`rounded-full mt-6 h-6 w-6 ml-1 ${calculateUrgency(
+              props.urgency
+            )}`}
+          ></div>
         </div>
         <div class="flex flex-col">
           <p class="text-lg font-bold">{props.subject}</p>
           <p class="text-sm w-full text-gray-500">{props.summary}</p>
         </div>
         <div className="">
-          <div className="tooltip tooltip-left h-[10]" data-tip={props.timestamp}>
+          <div
+            className="tooltip tooltip-left h-[10]"
+            data-tip={props.timestamp}
+          >
             <FontAwesomeIcon className="mr-1 mx-auto" icon={faCalendar} />
           </div>
         </div>
       </div>
     </a>
-
   );
 }
