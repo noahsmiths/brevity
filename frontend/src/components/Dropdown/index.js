dc416f0b27a6db0a07c262af102352d905d55fae
bgoulet1 <bgoulet@3cisd.com>
2022-11-05 23:11:26 -0400

    fixed dropdown

--- frontend/src/components/Dropdown/index.js
+++ frontend/src/components/Dropdown/index.js
@@ -1,28 +1,29 @@
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import React from "react";
 
 const Dropdown = () => {
   return (
-      <div className="dropdown dropdown-end border-black border-4">
-        <label tabIndex={0} className="btn btn-ghost rounded-btn">
-          <FontAwesomeIcon icon={faAngleDown} className="mr-1" />
-          Sort By
-        </label>
+    <div className="dropdown dropdown-bottom border-black border-4">
+      <label tabIndex={0} className="btn btn-ghost rounded-btn">
+        <FontAwesomeIcon icon={faAngleDown} className="mr-1" />
+        Sort By
+      </label>
+      <div>
         <ul
           tabIndex={0}
           className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4"
         >
           <li>
-            <div>Item 1</div>
+            <div>Recency</div>
           </li>
           <li>
-            <div>Item 2</div>
+            <div>Urgency</div>
           </li>
         </ul>
       </div>
-
+    </div>
   );
 };
 
