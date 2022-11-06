68cc09fae243adc0a689d8b7259a5e5841738797
Farouk Charkas <farouk.charkas@gmail.com>
2022-11-05 23:11:12 -0400

    init

--- frontend/src/components/EmailCard/index.js
+++ frontend/src/components/EmailCard/index.js
@@ -5,34 +5,56 @@ import React, { useEffect, useState } from "react";
 export default function EmailCard(props) {
   const subject = props.subject;
   const summary = props.summary;
-  const score = props.score;
-  const normMagnitude = props.normMagnitude;
+  const urgency = props.urgency
+  const timestamp = props.timestamp;
+  const sender = props.sender;
+  const url = props.url;
 
-  var color;
-  if (score > 0) {
-    color = "#4BB543";
-  } else if (score === 0) {
-    color = "#B8B8B8";
-  } else {
-    color = "#FF9494";
+  const [color, setColor] = useState("");
+
+  const calculateUrgency = (urgency) => {
+    switch (urgency) {
+      case 1:
+        setColor("bg-neutral-50");
+        break;
+      case 2:
+        setColor("bg-yellow-500");
+        break;
+      case 3:
+        setColor("bg-amber-500");
+        break;
+      case 4:
+        setColor("bg-orange-500");
+        break;
+      case 5:
+        setColor("bg-red-500");
+        break;
+      default:
+        setColor("bg-neutral-50");
+        break;
+    }
+
   }
 
+
+
   return (
-    <div className="card w-full mt-1 bg-base-100 border-0.2 border-softgray shadow-xl p-3 flex flex-row scale-[.975] hover:scale-100 hover:underline hover:cursor-pointer">
-      <div class="w-11 pr-2">
-        <div class="rounded-full mt-6 h-6 w-6 ml-1 bg-success"></div>
-      </div>
-      <div class="flex flex-col">
-        <p class="text-lg font-bold">Example subject</p>
-        <p class="text-sm w-full text-gray-500">fdkjkjb s sdjk snd ksjbd skjk sdn sldkns lkdns lkdnsl dns dlksn dlsknd jdbsk jb </p>
-      </div>
-      <div className="">
-        <div className="tooltip tooltip-left h-[10]" data-tip="January 24th, 2022">
-          <FontAwesomeIcon className="mr-1 mx-auto" icon={faCalendar} />
+    <a href={url}>
+      <div className="card w-full mt-1 bg-base-ß100 border-0.2 border-softgray shadow-xl p-3 flex flex-row scale-[.975] hover:scale-100 hover:underline hover:cursor-pointer">
+        <div class="w-11 pr-2">
+          <div class={`rounded-full mt-6 h-6 w-6 ml-1 ${calculateUrgency(urgency)}`}></div>
+        </div>
+        <div class="flex flex-col">
+          <p class="text-lg font-bold">{subject}</p>
+          <p class="text-sm w-full text-gray-500">{summary}</p>
+        </div>
+        <div className="">
+          <div className="tooltip tooltip-left h-[10]" data-tip={timestamp}>
+            <FontAwesomeIcon className="mr-1 mx-auto" icon={faCalendar} />
+          </div>
         </div>
       </div>
+    </a>
 
-
-    </div>
   );
 }
